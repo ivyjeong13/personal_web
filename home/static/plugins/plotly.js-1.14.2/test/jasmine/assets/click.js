ca4788bf5c8fe40ffa4af18b2500265cb774b11d
ijeong-nj <ijeong@nationaljournal.com>
2016-07-15 17:34:24 -0400

    remove images

--- home/static/plugins/plotly.js-1.14.2/test/jasmine/assets/click.js
+++
@@ -1,7 +0,0 @@
-var mouseEvent = require('./mouse_event');
-
-module.exports = function click(x, y) {
-    mouseEvent('mousemove', x, y);
-    mouseEvent('mousedown', x, y);
-    mouseEvent('mouseup', x, y);
-};
