6233e3831b038e25fa5118f1f051c893630b9bfd
ijeong-nj <ijeong@nationaljournal.com>
2016-07-15 17:33:35 -0400

    plotly builder

--- home/static/js/code.js
+++ home/static/js/code.js
@@ -5,3 +5,8 @@ $(document).on('mouseover', '.snp-banner a', function(){
 $(document).on('mouseout', '.snp-banner a', function(){
 	$('.snp-banner span').fadeOut().removeClass('animated pulse infinite');
 });
+
+$(document).on('click', '#sub-nav li', function(){
+	var id = $(this).attr('data-id');
+	$('html,body').animate({scrollTop: $('#'+id).offset().top - $('#nav').height() - 10}, 400);;
+});
