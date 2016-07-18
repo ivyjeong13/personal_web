40485f01c1b6a66547faef6e62d8c42d2d2ffbdc
ijeong-nj <ijeong@nationaljournal.com>
2016-07-18 16:48:07 -0400

    stuff

--- home/static/js/plotly_builder.js
+++ home/static/js/plotly_builder.js
@@ -1,19 +1,14 @@
+var colors =[
+	"#b5a286", "#beb3b7", "#7c8c87", "#cd6966", "#6fb2c7","#c2c2c2", "#559a87", 
+    "#8d734a", "#72666a", "#5e6c68", "#aa3e3d", "#3d7080", "#999999", "#366b5d",
+	"#cabca6","#e1d5d9", "#bbd0c7", "#e5a89b", "#add9e7", "#d1d1d1","#0d3a70",
+	"#b22830", "#dac11f"
+];
+
 $(document).ready(function(){
 	buildGraph();
-
-	$("#showPaletteOnly").spectrum({
-	    showPaletteOnly: true,
-	    togglePaletteOnly: true,
-	    togglePaletteMoreText: 'more',
-	    togglePaletteLessText: 'less',
-	    color: '#b5a286',
-	    palette: [
-	        ["#b5a286", "#beb3b7", "#7c8c87", "#cd6966", "#6fb2c7","#c2c2c2", "#559a87"], 
-	        ["#8d734a", "#72666a", "#5e6c68", "#aa3e3d", "#3d7080", "#999999", "#366b5d"],
-			["#cabca6","#e1d5d9", "#bbd0c7", "#e5a89b", "#add9e7", "#d1d1d1","#0d3a70"],
-			["#b22830", "#dac11f"]
-	    ]
-	});
+/**
+THIS IS FOR EXPORTING CSV AND XLSX FILES. PENDING.
 
 	var csvParser = new SimpleExcel.Parser.CSV();
     var fileInput = document.getElementById('fileInput');
@@ -48,35 +43,42 @@ $(document).ready(function(){
 			reader.readAsBinaryString(file);
         }
     });
+**/
 });
 
 function buildGraph(){
-	var trace1 = {
-	  x: [-1.0, -0.857142857143, -0.714285714286, -0.571428571429, -0.428571428571, -0.285714285714, -0.142857142857, 0.0, 0.142857142857, 0.285714285714, 0.428571428571, 0.571428571429, 0.714285714286, 0.857142857143, 1.0],
-	  y: [-1.0, -0.857142857143, -0.714285714286, -0.571428571429, -0.428571428571, -0.285714285714, -0.142857142857, 0.0, 0.142857142857, 0.285714285714, 0.428571428571, 0.571428571429, 0.714285714286, 0.857142857143, 1.0],
-	  ncontours: 30,
-	  showscale: false,
-	  type: 'line'
-	};
+	$('.plotly-block').each(function(){
+		var id = $(this).find('.dynamic-graph').attr('id');
 
-	var trace2 = {
-	  x: [-0.8, -0.48, -0.288, -0.1728, -0.10368, -0.062208, -0.0373248, -0.02239488, -0.013436928, -0.0080621568, -0.00483729408, -0.002902376448, -0.0017414258688, -0.00104485552128, -0.000626913312768, -0.000376147987661],
-	  y: [-0.9, -0.72, -0.576, -0.4608, -0.36864, -0.294912, -0.2359296, -0.18874368, -0.150994944, -0.1207959552, -0.09663676416, -0.077309411328, -0.0618475290624, -0.0494780232499, -0.0395824185999, -0.0316659348799],
-	  mode: 'markers+lines',
-	  name: 'steepest',
-	  line: {color: 'black'},
-	  type: 'scatter'
-	};
+		var $data = $(this).find('input[name="graph_data"]');
+		var $layout = $(this).find('input[name="graph_layout"]');
 
-	var data = [trace1, trace2];
+		var data = [];
+		var layout = {
+			margin: {
+				b:120
+			}
+		};
 
-	var layout = {
-		margin: {
-			b:120
+		if($data.val() && $data.val() != ''){
+			data = JSON.parse($data.val());
 		}
-	};
+		if($layout.val() && $layout.val() != ''){
+			layout = JSON.parse($layout.val());
+		}
 
-	Plotly.newPlot('myDiv', data, layout);
+		Plotly.newPlot(id, data, layout);
+	});
+}
+
+$(document).on('click', '.example-btn', function(){
+	buildExample($(this).parents('.plotly-block:first'));
+});
+
+function buildExample($chartblock){
+	var data = "date\tbananas\toranges\n2011\t1\t2\n2012\t4\t1\n2013\t2\t8\n2014\t6\t5\n2015\t3\t3\n2016\t8\t6.5\n"
+
+	$chartblock.find('textarea[name="database"]').text(data).trigger('change');
 }
 
 /**
@@ -96,7 +98,36 @@ $(document).on('click', '.plotly-block .hide-textarea-btn', function(){
 });
 
 $(document).on('change', 'textarea[name="database"]', function(){
-	console.log($(this).val());
+	var text = $(this).val();
+	var lines = text.split(/\n/);
+	var graph_data = [];
+	var x_axis;
+	for (l = 0; l < lines.length; l++){
+		var cols = lines[l].split(/\t/);
+
+		//first line, first col is names
+		//first name is the y-axis
+		if(l==0){
+			x_axis = cols[0];
+			for(c=0; c<cols.length; c++){
+				graph_data.push({'name': cols[c], 'x': [], 'y': []});
+			}
+		}else{
+			var x_val = cols[0];
+			for(c=0; c<cols.length; c++){
+				if(c!=0){
+					graph_data[c]['x'].push(x_val);
+					graph_data[c]['y'].push(cols[c]);
+				}
+			}
+		}
+	}
+
+	var chart = getChart($(this));
+	var $chartblock = $(this).parents('.plotly-block:first');
+
+	updateGraphData(chart, $chartblock, graph_data);
+	setData($(this), chart);
 });
 
 $(document).delegate('textarea[name="database"]', 'keydown', function(e) {
@@ -118,11 +149,13 @@ $(document).delegate('textarea[name="database"]', 'keydown', function(e) {
   }
 });
 
-$(document).on('change', 'input[name="title"]', function(){
+$(document).on('change', 'input[name="title_g"]', function(){
 	var chart = getChart($(this));
 
 	chart.layout.title = $(this).val();
+
 	Plotly.redraw(chart);
+	setData($(this), chart);
 });
 
 $(document).on('change', 'input[name="source"]', function(){
@@ -133,7 +166,9 @@ $(document).on('change', 'input[name="source"]', function(){
 	chart.layout.xaxis = {
 		title: xtitle + '<br><br> <i>Source:'+$(this).val() + '</i><br> NationalJournal | Axis: Data & Charts Hub'
 	};
+
 	Plotly.redraw(chart);
+	setData($(this), chart);
 });
 
 $(document).on('change', 'input[name="x-title"], input[name="y-title"]', function(){
@@ -152,6 +187,59 @@ $(document).on('change', 'input[name="x-title"], input[name="y-title"]', functio
 	}
 
 	Plotly.redraw(chart);
+	setData($(this), chart);
+});
+
+$(document).on('change', 'input[name="series_color"]', function(){
+	var color = $(this).val();
+	var chart = getChart($(this));
+	var num = $(this).parent('li:first').attr('data-num');
+	var $chartblock = $(this).parents('.plotly-block:first');
+
+	chart.data[num]['marker']['color'] = color;
+
+	Plotly.redraw(chart);
+	setData($(this), chart);
+});
+
+$(document).on('change', 'select[name="series_type"]', function(){
+	var val = $(this).val();
+	var num = $(this).parent('li:first').attr('data-num');
+	var chart = getChart($(this));
+	var reversed = false;
+
+	if(chart.data[num]['type'] == 'bar' && chart.data[num]['orientation'] == 'h')
+		reversed = true;
+
+	var x_s = chart.data[num]['x'];
+	var y_s = chart.data[num]['y'];
+
+	if(val == 'horizontal'){
+		chart.data[num]['type'] = 'bar';
+		chart.data[num]['orientation'] = 'h';
+		chart.data[num]['x'] = y_s;
+		chart.data[num]['y'] = x_s;
+	}
+	else{
+		if(reversed){
+			chart.data[num]['x'] = y_s;
+			chart.data[num]['y'] = x_s;
+		}
+		if(val == 'column'){
+			chart.data[num]['type'] = 'bar';
+			chart.data[num]['orientation'] = 'v';
+		}
+		else if(val == 'line'){
+			chart.data[num]['type'] = 'line';
+			chart.data[num]['mode'] = 'lines';
+		}
+		else if(val == 'scatter'){
+			chart.data[num]['type'] = 'line';
+			chart.data[num]['mode'] = 'markers';
+		}
+	}
+	Plotly.redraw(chart);
+	setData($(this), chart);
 });
 
 $(document).on('change', 'input[name="x-num_ticks"], input[name="y-num_ticks"]', function(){
@@ -167,6 +255,7 @@ $(document).on('change', 'input[name="x-num_ticks"], input[name="y-num_ticks"]',
 	}
 
 	Plotly.redraw(chart);
+	setData($(this), chart);
 });
 
 var ps_classes = ['input[name="x-prefix"]', 'input[name="x-suffix"]', 'input[name="y-prefix"]', 'input[name="y-suffix"]'];
@@ -182,6 +271,7 @@ $(document).on('change', ps_classes.join(','), function(){
 	chart = changeSuffixPrefix(chart, isX, isPrefix, val, show);
 
 	Plotly.redraw(chart);
+	setData($(this), chart);
 });
 
 var ps_first_classes = ['input[name="x-prefix-first"]', 'input[name="x-suffix-first"], input[name="y-prefix-first"]', 'input[name="y-suffix-first"]']
@@ -208,6 +298,7 @@ $(document).on('change', ps_first_classes.join(','), function(){
 	chart = changeSuffixPrefix(chart, isX, isPrefix, val, show);
 
 	Plotly.redraw(chart);
+	setData($(this), chart);
 });
 
 $(document).on('change', 'input[name="x-max"], input[name="x-min"], input[name="y-max"], input[name="y-min"]', function(){
@@ -234,7 +325,68 @@ $(document).on('change', 'input[name="x-max"], input[name="x-min"], input[name="
 	}
 
 	Plotly.relayout(chart, update);
+	setData($(this), chart);
 });
+
+function updateGraphData(chart, $chartblock, data){
+	var traces = [];
+	var series = [];
+
+	if(data.length > 1){
+		for(d=1; d<data.length; d++){
+			var trace = {
+				x: data[d]['x'],
+				y: data[d]['y'],
+				type: 'line',
+				mode: 'lines',
+				name: data[d]['name'],
+				marker: {
+					color: colors[d],
+					//size: 12,
+				}
+			}
+			traces.push(trace);
+			series.push({'name': data[d]['name'], 'num': d-1, 'color': colors[d]});
+		}
+
+		addSeriesOption(series, $chartblock);
+	}
+
+	chart.data=traces;
+	Plotly.redraw(chart);
+}
+
+function addSeriesOption(series, $chartblock){
+	$chartblock.find('.list_series').html('');
+
+	for(s=0; s<series.length; s++){
+		var html = '<li data-num="'+series[s]['num']+'">';
+		html+='<span>'+series[s]['name']+'</span>';
+		html+='<input name="series_color" type="text" id="series_'+series[s]['num']+'">';
+		html+='<select name="series_type">';
+		html+='<option value="line">Line</option>';
+		html+='<option value="scatter">Scatter</option>';
+		html+='<option value="horizontal">Bar</option>';
+		html+='<option value="column">Column</option>';
+		html+='</select>';
+		html+='</li>';
+		$chartblock.find('.list_series').append(html);
+
+		$("#series_"+series[s]['num']).spectrum({
+		    showPaletteOnly: true,
+		    togglePaletteOnly: true,
+		    togglePaletteMoreText: 'more',
+		    togglePaletteLessText: 'less',
+		    color: series[s]['color'],
+		    palette: [
+		        ["#b5a286", "#beb3b7", "#7c8c87", "#cd6966", "#6fb2c7","#c2c2c2", "#559a87"], 
+		        ["#8d734a", "#72666a", "#5e6c68", "#aa3e3d", "#3d7080", "#999999", "#366b5d"],
+				["#cabca6","#e1d5d9", "#bbd0c7", "#e5a89b", "#add9e7", "#d1d1d1","#0d3a70"],
+				["#b22830", "#dac11f"]
+		    ]
+		});
+	}
+}
 
 /**
 	Will change the suffix or prefix for the specified axis.
@@ -264,4 +416,10 @@ function changeSuffixPrefix(chart, isX, isPrefix, val, show){
 function getChart($this){
 	var $chartblock = $this.parents('.plotly-block:first');
 	return document.getElementById($chartblock.find('.dynamic-graph').attr('id'));
+}
+
+function setData($this, chart){
+	var $chartblock = $this.parents('.plotly-block:first');
+	$chartblock.find('input[name="graph_layout"]').attr('value', JSON.stringify(chart.layout));
+	$chartblock.find('input[name="graph_data"]').attr('value', JSON.stringify(chart.data));
 }
